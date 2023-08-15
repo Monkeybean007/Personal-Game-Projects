82e57c96e9d4fe2801172bf5e08e5c0aad90746e
Seth Corbran <monkeybean1464@gmail.com>
2023-08-15 08:39:54 -0600

    Added Visual Studio
    Added a C++ class to the project.

---
+++ ObstacleAssault/Intermediate/Build/Win64/ObstacleAssaultEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.37.32822
+#include "C:/Users/monke/OneDrive/Desktop/GameProjects2/Personal-Game-Projects/ObstacleAssault/Intermediate/Build/Win64/ObstacleAssaultEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.h"
